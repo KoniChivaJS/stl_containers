de5ea9990342b09c7218255e2631d9652c5fd3d0
KoniChivaJS <mredik200@gmail.com>
2024-04-22 21:55:10 +0300

    Task 1 complete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <array>
 using namespace std;
+
+template <typename T>
+array<T,10> sortArr(array<T,5>& fArr, array<T,5>& sArr){
+    array<T,10> newArr;
+    auto itArr = newArr.begin();
+    auto it = fArr.begin();
+    auto itSecond = sArr.begin();
+    while(it!=fArr.end()){
+        if(*it<*itSecond){
+            *itArr = *it;
+            cout<<*itArr<<" ";
+            itArr++;
+            it++;
+        }else{
+            *itArr = *itSecond;
+            cout<<*itArr<<" ";
+            itArr++;
+            itSecond++;
+        }
+    }
+    return newArr;
+};
+
 int main() {
+
+//    Task 1
+    array<int,5> firstArr = {1,4,6,9,10};
+    array<int,5> secondArr = {0,2,4,7,8};
+    array<int,10> finalArr = sortArr(firstArr,secondArr);
+
+
 
     return 0;
 }
